4b4aef868ef8dbce506f3c290672d8ee972ea479
Soumyadev Sanyal <soumya.sp35@gmail.com>
2021-12-21 10:50:02 +0530

    About game and about developper added in the help menu which which will be displayed through an alert dialog box.

---
+++ Controller.java
@@ -0,0 +1,31 @@
+package com.SoumyadevSanyal.connectfour;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+	@FXML
+	public GridPane rootGridPane;
+
+	@FXML
+	public Pane insertedDiscsPane;
+
+	@FXML
+	public Label playerNameLabel;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+}
---
+++ Main.java
@@ -0,0 +1,117 @@
+package com.SoumyadevSanyal.connectfour;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import sun.plugin2.applet.FXAppletSecurityManager;
+
+public class Main extends Application {
+
+	private Controller controller;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("game.fxml"));
+		GridPane rootGridPane = loader.load();
+
+		controller = loader.getController();
+
+		MenuBar menuBar = createMenu();
+		menuBar.prefWidthProperty().bind(primaryStage.widthProperty()); // to make the menuBar take the width of the primaryStage
+
+		Pane menuPane = (Pane) rootGridPane.getChildren().get(0);
+
+		menuPane.getChildren().addAll(menuBar);
+
+		Scene scene = new Scene(rootGridPane);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Connect 4");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+
+	private MenuBar createMenu() {
+		// File menu
+		Menu fileMenu = new Menu("File");
+
+		MenuItem newGame = new MenuItem("New Game");
+		newGame.setOnAction(event -> resetGame());
+
+		MenuItem resetGame = new MenuItem("Reset Game");
+		resetGame.setOnAction(event -> resetGame());
+
+		SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+		MenuItem exitGame = new MenuItem("Exit");
+		exitGame.setOnAction(event -> {
+			exitGame();
+		});
+
+		fileMenu.getItems().addAll(newGame, resetGame, separatorMenuItem, exitGame);
+
+		// Help Menu
+		Menu helpMenu = new Menu("Help");
+
+		MenuItem aboutGame = new MenuItem("About Game");
+		aboutGame.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				aboutGame();
+			}
+		});
+
+
+		MenuItem aboutDeveloper = new MenuItem("About Developer");
+		aboutDeveloper.setOnAction(event -> aboutDeveloper());
+
+
+		SeparatorMenuItem separatorMenuItem1 = new SeparatorMenuItem();
+
+		helpMenu.getItems().addAll(aboutGame, separatorMenuItem1, aboutDeveloper);
+
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(fileMenu, helpMenu);
+
+		return menuBar;
+	}
+
+    private void aboutDeveloper() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About the Developer");
+        alert.setHeaderText("Soumyadev Sanyal");
+        alert.setContentText("Hi! My name is Soumyadev Sanyal." +
+                " I am an engineer specialised in Computer Science Engineering." +
+                " This game is my first project in JavaFX application." +
+                " I made it when I was undergoing a core java training from Internshala." +
+                " Thanks for reading.");
+        alert.show();
+    }
+
+    private void aboutGame() {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("About the game");
+		alert.setHeaderText("How to play");
+		alert.setContentText("Connect Four is a two-player connection game in which the players first choose a color and then take turns dropping colored discs from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the next available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs. Connect Four is a solved game. The first player can always win by playing the right moves.");
+		alert.show();
+	}
+
+	private void exitGame() {
+		Platform.exit();
+		System.exit(0);
+	}
+
+	private void resetGame() {
+	}
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
